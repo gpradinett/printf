82a37f4f4fe55b673aa59cd827224037fe746044
juan20carlos <juancarlos14089@gmail.com>
2022-03-15 21:18:02 +0000

    Error correction

--- verificador.c
+++ verificador.c
@@ -1,7 +1,11 @@
 #include "main.h"
-
+/**
+ * print_perc - print a porcent sing (%)
+ * @p: pointer to argument
+ * Return: Nothing
+ */
 int print_perc(va_list __attribute__((unused)) p)
 {
-			_putchar('%');
-			return (1);
+	_putchar('%');
+	return (1);
 }
